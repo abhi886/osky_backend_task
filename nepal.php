fc15fa2d03ceb03ca03205a0a78eb11693316952
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2023-08-03 09:36:40 +1000

    3. Created a listing of the news wrapped inside a div and sort it according to the news title alphabetically.

--- nepal.php
+++ nepal.php
@@ -2,7 +2,18 @@
 // Fetch the JSON content from the URL
 $json_url = "https://test.osky.dev/101/data.json";
 $data = file_get_contents($json_url);
-$result = json_decode($data, true);
+$newsData = json_decode($data, true);
+
+// Custom comparison function for sorting by title
+function sortByTitle($a, $b) {
+    return strcmp($a['title'], $b['title']);
+}
+
+// Sort the news data array based on title
+usort($newsData, 'sortByTitle');
+
+print_r($newsData);
+
  // Display all the titles
 echo "<div>";
 foreach($result as $data) {
